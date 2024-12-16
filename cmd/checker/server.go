1dd9581fa21381d2a37edce15c2c96ae6a55896a
DDDDOGE <zhangguyu6@gmail.com>
2024-12-16 18:09:22 +0800

    feat: support jsonrpc resp (#18)

--- cmd/checker/server.go
+++ cmd/checker/server.go
@@ -8,107 +8,142 @@ import (
 	"github.com/Chaintable/consistency-checker/db"
 	"github.com/Chaintable/consistency-checker/nodes"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/gin-gonic/gin"
 )
 
+type JsonRpcRsp struct {
+	ID      int         `json:"id"`
+	JsonRpc string      `json:"jsonrpc"`
+	Result  interface{} `json:"result,omitempty"`
+	Error   *ErrorRsp   `json:"error,omitempty"`
+}
+
+type ErrorRsp struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func NewJsonRpcRsp(result interface{}) *JsonRpcRsp {
+	return &JsonRpcRsp{
+		ID:      1,
+		JsonRpc: "2.0",
+		Result:  result,
+	}
+}
+
+func NewErrorRsp(code int, message string) *JsonRpcRsp {
+	return &JsonRpcRsp{
+		ID:      1,
+		JsonRpc: "2.0",
+		Error: &ErrorRsp{
+			Code:    code,
+			Message: message,
+		},
+	}
+}
+
+type BlockContext struct {
+	BlockId *rpc.BlockNumberOrHash `json:"block_id"`
+	Type    string                 `json:"type"`
+}
+
 func handleGetLatestBlock(c *gin.Context) {
 	if db.DB == nil {
-		c.JSON(-39005, gin.H{"error": "db not initialized"})
+		c.JSON(200, NewErrorRsp(-39005, "db not initialized"))
 		return
 	}
 
 	latestBlock, err := db.DB.GetLatestBlockInfo()
 	if err != nil {
-		c.JSON(-39005, gin.H{"error": err.Error()})
+		c.JSON(200, NewErrorRsp(-39005, err.Error()))
 		return
 	}
 
-	c.JSON(200, latestBlock)
+	c.JSON(200, NewJsonRpcRsp(latestBlock))
 }
 
 func handleGetBlockByHeight(c *gin.Context, req *nodes.JsonRpcReq) {
 	if len(req.Params) == 0 {
-		c.IndentedJSON(-32602, "params not found")
+		c.JSON(200, NewErrorRsp(-32602, "params not found"))
 		return
 	}
 	height := req.Params[0].(string)
 	if height == "" {
-		c.IndentedJSON(-32602, "params not found")
+		c.JSON(200, NewErrorRsp(-32602, "params not found"))
 		return
 	}
 
 	num := new(big.Int)
-	num, ok := num.SetString(height, 10)
+	num, ok := num.SetString(height, 0)
 	if !ok {
-		c.IndentedJSON(-32602, "params error")
+		c.JSON(200, NewErrorRsp(-32602, "num params error"))
 		return
 	}
 
 	if db.DB == nil {
-		c.JSON(-39005, gin.H{"error": "db not initialized"})
+		c.JSON(200, NewErrorRsp(-39005, "db not initialized"))
 		return
 	}
 
 	block, err := db.DB.GetBlockInfoByNum(num)
 	if err != nil {
-		c.IndentedJSON(-39005, err.Error())
+		c.JSON(200, NewErrorRsp(-39005, err.Error()))
 		return
 	}
-	c.IndentedJSON(200, block)
+	c.JSON(200, NewJsonRpcRsp(block))
 }
 
 func handleGetBlockById(c *gin.Context, req *nodes.JsonRpcReq) {
 	if len(req.Params) == 0 {
-		c.IndentedJSON(-32602, "params not found")
+		c.JSON(200, NewErrorRsp(-32602, "params not found"))
 		return
 	}
-	hash := req.Params[0].(string)
-	if hash == "" {
-		c.IndentedJSON(-32602, "params not found")
+	id, ok := req.Params[0].(string)
+	if !ok {
+		c.JSON(200, NewErrorRsp(-32602, "params error"))
 		return
 	}
-
 	if db.DB == nil {
-		c.JSON(-39005, gin.H{"error": "db not initialized"})
+		c.JSON(200, NewErrorRsp(-39005, "db not initialized"))
 		return
 	}
 
-	block, err := db.DB.GetBlockInfoByHash(common.HexToHash(hash))
+	block, err := db.DB.GetBlockInfoByHash(common.HexToHash(id))
 	if err != nil {
-		c.IndentedJSON(-39005, err.Error())
+		c.JSON(200, NewErrorRsp(-39005, err.Error()))
 		return
 	}
-	c.IndentedJSON(200, block)
+	c.IndentedJSON(200, NewJsonRpcRsp(block))
 }
 
 func handleBlockIsValid(c *gin.Context, req *nodes.JsonRpcReq) {
 	if len(req.Params) == 0 {
-		c.IndentedJSON(-32602, "params not found")
+		c.JSON(200, NewErrorRsp(-32602, "params not found"))
 		return
 	}
-	hash := req.Params[0].(string)
-	if hash == "" {
-		c.IndentedJSON(-32602, "params not found")
+	id, ok := req.Params[0].(string)
+	if !ok {
+		c.JSON(200, NewErrorRsp(-32602, "params error"))
 		return
 	}
-
 	if db.DB == nil {
-		c.JSON(-39005, gin.H{"error": "db not initialized"})
+		c.JSON(200, NewErrorRsp(-39005, "db not initialized"))
 		return
 	}
 
-	block0, err := db.DB.GetBlockInfoByHash(common.HexToHash(hash))
+	block0, err := db.DB.GetBlockInfoByHash(common.HexToHash(id))
 	if err != nil {
-		c.IndentedJSON(-39005, err.Error())
+		c.JSON(200, NewErrorRsp(-39005, err.Error()))
 		return
 	}
 
 	block1, err := db.DB.GetBlockInfoByNum(big.NewInt(0).SetUint64(block0.Height))
 	if err != nil {
-		c.IndentedJSON(-39005, err.Error())
+		c.JSON(200, NewErrorRsp(-39005, err.Error()))
 		return
 	}
-	c.IndentedJSON(200, block0.ID == block1.ID)
+	c.IndentedJSON(200, NewJsonRpcRsp(block0.ID == block1.ID))
 }
 
 func index(c *gin.Context) {
--- go.mod
+++ go.mod
@@ -15,6 +15,8 @@ require (
 
 require (
 	github.com/DataDog/zstd v1.5.6 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.32.5 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.6 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.28.5 // indirect
@@ -47,11 +49,13 @@ require (
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.1.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/ethereum/c-kzg-4844 v1.0.3 // indirect
 	github.com/ethereum/go-verkle v0.2.2 // indirect
 	github.com/getsentry/sentry-go v0.29.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
@@ -59,6 +63,7 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/holiman/uint256 v1.3.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
@@ -78,7 +83,10 @@ require (
 	github.com/prometheus/common v0.60.1 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/supranational/blst v0.3.13 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.17 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.17 // indirect
